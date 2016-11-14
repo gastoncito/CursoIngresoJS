72c3b58d3c0cbfe2e56535007e0095d746d42f64
gastoncito <gaston-grande@outlook.com>
2016-11-14 11:41:40 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,10 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+//la ventanaalert() mensaje emergente
+alert("esto funciona de maravilla");
+	// literal, mensaje de texto
+
+}
+
+
---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,12 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{var apellido = "GRANDE";
+	var nombre;
+	nombre="GASTON";
+	nombre= prompt ("ingrese su nombre","N N");
+	alert("mi nombre es : " + nombre + " y su apellido es "+ apellido);
+	
+}
+
+
