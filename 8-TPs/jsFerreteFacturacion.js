7396b92d9c6807c709059ac86b3580bf75a8ae33
gastoncito <gaston-grande@outlook.com>
2016-11-18 12:02:09 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -4,7 +4,20 @@ B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
-{
+{var precio1;
+		var precio2;
+		var precio3;
+		var sumar;
+		precio1=document.getElementById('PrecioUno').value;
+		precio1=parseInt(precio1);
+		precio2=document.getElementById('PrecioDos').value;
+		precio2=parseInt(precio2);
+        precio3=document.getElementById('PrecioTres').value;
+        precio3=parseInt(precio3);
+
+		sumar=precio1+precio2+precio3;
+		alert("la suma de los precios es "+ sumar);
+	
 	
 }
 function Promedio () 
